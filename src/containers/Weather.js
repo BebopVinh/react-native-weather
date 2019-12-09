90319841fc21c44aba65ec599ec24855a666456c
bebopvinh <vinhdo07@gmail.com>
2019-12-09 11:14:00 -0800

    Initial Push

---
+++ src/containers/Weather.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Image } from 'react-native'
+
+export default function Weather() {
+    let pic = { uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }
+    return (
+        <Image source={pic} style={{ width: 193, height: 110 }} />
+    )
+}
+
